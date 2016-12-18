ab264f5e316d1f99dbba6dab1603c6bad0752b2a
Robert Sundström <robert.sundstrom@outlook.com>
2016-12-18 15:25:00 +0100

    Initial commit.

---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+# Common Service Locator
+
+Provides a common interface and service locator for leveraging [Inversion of Control](https://en.wikipedia.org/wiki/Inversion_of_control) (IoC).
+
+This is essentially an identical fork of the "Common Service Locator", orginally created by [Microsoft Patterns and Practices](https://msdn.microsoft.com/en-us/library/ff921345.aspx) and hosted on [Codeplex](http://commonservicelocator.codeplex.com/).
+
+The project targets [.NET Standard](https://github.com/dotnet/standard) 1.6.
---
+++ test/Microsoft.Practices.ServiceLocation.Tests/ServiceLocatorFixture.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Practices.ServiceLocation;
+using ServiceLocation.Tests.Mocks;
+using Xunit;
+
+namespace ServiceLocation.Tests
+{
+    public class ServiceLocatorFixture
+    {
+        [Fact]
+        public void TestInit()
+        {
+            ServiceLocator.SetLocatorProvider(null);
+        }
+
+        [Fact]
+        public void ServiceLocatorIsLocationProviderSetReturnsTrueWhenSet()
+        {
+            ServiceLocator.SetLocatorProvider(() => new MockServiceLocator());
+
+            Assert.True(ServiceLocator.IsLocationProviderSet);
+        }
+
+        [Fact]
+        public void ServiceLocatorIsLocationProviderSetReturnsFalseWhenNotSet()
+        {
+            Assert.False(ServiceLocator.IsLocationProviderSet);
+        }
+
+        [Fact]
+        public void ServiceLocatorCurrentThrowsWhenLocationProviderNotSet()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                var currentServiceLocator = ServiceLocator.Current;
+            });
+        }
+    }
+}
